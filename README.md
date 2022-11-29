bcb8794bec4a298e6cd538d1c03b95eb76ac7c77
Yuki Uranishi <uranishi@gmail.com>
2022-11-29 13:15:08 +0900

    files added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # colab_cameracapture
+
+## Outline
+repository for pip installation 
+
+## Thanks to
+[Colab上でwebカメラをリアルタイムに処理](https://qiita.com/a2kiti/items/f32de4f51a31d609e5a5)
---
+++ setup.py
@@ -0,0 +1,30 @@
+#
+# pip setup
+# Thanks to: https://dev.classmethod.jp/articles/pip-install-via-github-command/
+#
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colab_cameracapture",
+    version="0.1.0",
+    author="uranishi",
+    author_email="uranishi@gmail.com",
+    description="Colab camera capture module with pip installation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uranishi/colab_cameracapture",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['colab_cameracapture = colab_cameracapture.colab_cameracapture:main']
+    },
+    python_requires='>=3.7',
+)
