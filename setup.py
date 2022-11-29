0f17a4018433c593668074040d6f86307eaf1f83
Yuki Uranishi <uranishi@gmail.com>
2022-11-29 13:25:56 +0900

    entry point removed

--- setup.py
+++ setup.py
@@ -23,8 +23,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points = {
-        'console_scripts': ['colab_cameracapture = colab_cameracapture.colab_cameracapture:main']
-    },
-    python_requires='>=3.7',
+   python_requires='>=3.7',
 )
